53457a015388501810be7bc9034b069c8b5ca2cd
Fatemeh-sr <Fatemeh.srvni8@gmail.com>
2025-12-29 02:50:26 +0330

    TfidfVectorizer

--- Email-Spam-Detection.py
+++ Email-Spam-Detection.py
@@ -1,4 +1,27 @@
 import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
 
-df = pd.read_csv("enron_spam_data.csv")
-df.info()
+
+def main():
+    df = pd.read_csv("enron_spam_data.csv")
+    df.info()
+
+    df["Subject"] = df["Subject"].fillna("")
+    df["Message"] = df["Message"].fillna("")
+
+    print(df.isnull().sum())
+    # print(df)
+
+    # vectorizer = TfidfVectorizer()
+    vectorizer = TfidfVectorizer(stop_words="english")  # max_features=8000,
+
+    # df["text"] = df["Subject"] + " " + df["Message"]
+    # X = vectorizer.fit_transform(df["text"])
+
+    texts = df["Subject"].str.cat(df["Message"], sep=" ")
+    X = vectorizer.fit_transform(texts)
+    print("TF-IDF shape:", X.shape)  # 33,716 ایمیل     n ویژگی (کلمه مهم)
+
+
+if __name__ == "__main__":
+    main()
