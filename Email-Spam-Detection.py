181fe9a0f10116c3e19788eb904882645af6ad79
Fatemeh-sr <Fatemeh.srvni8@gmail.com>
2026-01-06 03:02:09 +0330

    update code -- def new_email

--- Email-Spam-Detection.py
+++ Email-Spam-Detection.py
@@ -3,11 +3,10 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.neural_network import MLPClassifier
 from sklearn import metrics
-import matplotlib.pyplot as plt
 from sklearn.naive_bayes import MultinomialNB
 
 
-def train_evaluate_mlp(X_train, X_test, y_train, y_test):
+def train_MLPClassifier(X_train, X_test, y_train, y_test):
     mlp = MLPClassifier(
         hidden_layer_sizes=(100,),
         max_iter=300,
@@ -20,32 +19,52 @@ def train_evaluate_mlp(X_train, X_test, y_train, y_test):
     mlp.fit(X_train, y_train)
 
     y_prediction = mlp.predict(X_test)
-    y_prob = mlp.predict_proba(X_test)[:, 1]
 
-    print("------------- MLP -------------")
+    print("------------- MLP -------------\n")
     print("Accuracy:", metrics.accuracy_score(y_test, y_prediction))
     print("Precision:", metrics.precision_score(y_test, y_prediction))
     print("Recall:", metrics.recall_score(y_test, y_prediction))
     print("F1:", metrics.f1_score(y_test, y_prediction))
     print(metrics.classification_report(y_test, y_prediction))
 
-    return y_prediction, y_prob
+    return mlp
 
 
-def train_evaluate_mnb(X_train, X_test, y_train, y_test):
+def train_MultinomialNB(X_train, X_test, y_train, y_test):
     multinomialNB = MultinomialNB()
     multinomialNB.fit(X_train, y_train)
 
     y_pred = multinomialNB.predict(X_test)
 
-    print("------------- Multinomial Naive Bayes -------------")
+    print("------------- Multinomial Naive Bayes -------------\n")
     print("Accuracy:", metrics.accuracy_score(y_test, y_pred))
     print("Precision:", metrics.precision_score(y_test, y_pred))
     print("Recall:", metrics.recall_score(y_test, y_pred))
     print("F1:", metrics.f1_score(y_test, y_pred))
     print(metrics.classification_report(y_test, y_pred))
 
-    return y_pred
+    return multinomialNB
+
+
+def predict_new_email(file_path, vectorizer, mlp_model, mnb_model):
+    try:
+        with open(file_path, encoding="utf-8") as my_file:
+            email_text = my_file.read()
+
+        email_vector = vectorizer.transform([email_text])
+
+        mlp_pred = mlp_model.predict(email_vector)[0]
+        mlp_label = "spam" if mlp_pred == 1 else "ham"
+        print(f"MLP Prediction: {mlp_label}")
+
+        mnb_pred = mnb_model.predict(email_vector)[0]
+        mnb_label = "spam" if mnb_pred == 1 else "ham"
+        print(f"Naive Bayes Prediction: {mnb_label}")
+
+    except FileNotFoundError:
+        print(f"File '{file_path}' not found.")
+    except Exception as e:
+        print("Error:", e)
 
 
 def main():
@@ -61,36 +80,19 @@ def main():
 
     y = df["Spam/Ham"].map({"ham": 0, "spam": 1})
 
-    X_train, X_test, y_train, y_test = train_test_split(
-        X, y, train_size=0.7, random_state=42, stratify=y
+    X_train_text, X_test_text, y_train, y_test = train_test_split(
+        texts, y, train_size=0.7, random_state=42, stratify=y
     )
 
+    X_train = vectorizer.fit_transform(X_train_text)
+    X_test = vectorizer.transform(X_test_text)
+
     # MLP
-    y_pred_mlp, y_prob_mlp = train_evaluate_mlp(X_train, X_test, y_train, y_test)
-
     # Naive Bayes
-    train_evaluate_mnb(X_train, X_test, y_train, y_test)
+    mlp_model = train_MLPClassifier(X_train, X_test, y_train, y_test)
+    mnb_model = train_MultinomialNB(X_train, X_test, y_train, y_test)
 
-    # ROC Curve comparison for MLP and Naive Bayes
-
-    # احتمال کلاس 1
-    y_prob_mnb = mnb.predict_proba(X_test)[:, 1]
-
-    fpr_mlp, tpr_mlp, _ = metrics.roc_curve(y_test, y_prob_mlp)
-    auc_mlp = metrics.roc_auc_score(y_test, y_prob_mlp)
-
-    fpr_nb, tpr_nb, _ = metrics.roc_curve(y_test, y_prob_mnb)
-    auc_nb = metrics.roc_auc_score(y_test, y_prob_mnb)
-
-    plt.figure()
-    plt.plot(fpr_mlp, tpr_mlp, label=f"MLP (AUC = {auc_mlp:.3f})")
-    plt.plot(fpr_nb, tpr_nb, label=f"Naive Bayes (AUC = {auc_nb:.3f})")
-    plt.plot([0, 1], [0, 1], linestyle="--")  # خط تصادفی
-    plt.xlabel("False Positive Rate")
-    plt.ylabel("True Positive Rate")
-    plt.title("ROC Curve Comparison")
-    plt.legend()
-    plt.show()
+    # predict_new_email("new_email.txt", vectorizer, mlp_model, mnb_model)
 
 
 if __name__ == "__main__":
